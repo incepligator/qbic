1ebd4c63eb71ab7f1f4996fe38f0d034b1725185
Sanjaya <sunzpdl@gmail.com>
2017-02-02 12:43:37 -0600

    added java file

---
+++ GitMerge.java
@@ -0,0 +1,20 @@
+package assignmentLokeshOOPS;
+public class GitMerge {
+
+	
+	public static void main(String[] args) {
+		
+         GitMerge gt = new GitMerge();
+         gt.Display();
+	}
+	
+	static void Display()
+	{
+		System.out.println("Arjun");
+		System.out.println("Sudeep");
+		
+		System.out.println("Shrawan"); // Shrawan 
+		System.out.println("Sanjaya");
+	}
+
+}
